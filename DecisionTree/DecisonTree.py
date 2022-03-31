12054a62d6926c0905551fba0f3a7445e3fa93cc
Nunyapa <halo.cola.115@gmail.com>
2022-03-31 22:34:50 +1000

    Add information gain, gini impurity

--- DecisionTree/DecisonTree.py
+++ DecisionTree/DecisonTree.py
@@ -1,11 +1,11 @@
 import numpy as np
+from typing import Iterable, Mapping, TypeVar
 
-def _entropy(probabilities):
-    result = [p * np.log(p) for p in probabilities]
-    result = -1 * np.sum(result)
-    return result
+T = TypeVar('T')
 
-def _information_gain()
+# TODO : move all metrics to another file
+# TODO : make regression metrics
+# 
 
 
 class DecisionTree():
@@ -28,31 +28,55 @@ class DecisionTree():
     @staticmethod
     def _get_fractions(values):
         uniqs, freqs = np.unique(values, return_counts=True)
-        amount = p.sum(freqs)
+        amount = np.sum(freqs)
         freqs = freqs / amount
         return uniqs, freqs, amount 
 
     @staticmethod
-    def _split(sorted_mapping, split_value):
-        # assert split_value in features_values
-
-        # mapping = list(zip(features_values, target_values))
-        # mapping = sorted(mapping, key=lambda x: x[0])
+    def _split(sorted_mapping: Mapping[T, T], split_value: T):
         for idx in range(len(sorted_mapping)):
             if split_value < sorted_mapping[idx][0]:
                 break
         return sorted_mapping[:idx], sorted_mapping[idx]
 
-    def _find_best_split(features_values, target_values):
+    @staticmethod
+    def _entropy(probabilities: Iterable[float]) -> float:
+        result = [p * np.log(p) for p in probabilities]
+        result = -1 * np.sum(result)
+        return result
+
+    def _gini_impurity(self, left_probs: Iterable[float],  right_probs: Iterable[float]):
+        left_probs_size = len(left_probs)
+        right_probs_size = len(right_probs)
+        size = left_probs_size + right_probs_size
+
+        left_gini = 2 * left_probs / left_probs_size
+        right_gini = 2 * right_probs / right_probs_size
+        gini_impurity = (left_probs_size / size) * left_gini + (right_probs_size / size) * right_gini
+        
+        return gini_impurity
+        
+    def _information_gain(self, parent_probs: Iterable[float], children_probs: Iterable[Iterable[float], Iterable[float]]) -> float:
+        ig = self._entropy(parent_probs)
+        for child_prob in children_probs:
+            ig -= len(child_prob) * self._entropy(child_prob) / len(parent_probs)
+        return ig
+
+    def _find_best_split(self, features_values: Iterable[float], target_values: Iterable[float]):
         assert len(features_values) == len(target_values)
         mapping = list(zip(features_values, target_values))
         mapping = sorted(mapping, key=lambda x: x[0])
-        for split, _ in mapping:
-            left_sample, right_sample = self._split(mapping, split)
-            _, left_freqs = self._get_fractions(_get_column_from_map(left_sample, col_idx=1))
-            _, right_freqs = self._get_fractions(_get_column_from_map(right_sample, col_idx=1))
-
-            left_entropy = self._entropy(left_freqs)
-            right_entropy = self._entropy(right_freqs)
-
+        best_ig = -1
+        best_split = None
+        for f_value, _ in mapping:
+            left_sample, right_sample = self._split(mapping, f_value)
+            _, _, common_freqs = self._get_fractions(self._get_column_from_map(mapping, col_idx=1))
+            _, _, left_freqs = self._get_fractions(self._get_column_from_map(left_sample, col_idx=1))
+            _, _, right_freqs = self._get_fractions(self._get_column_from_map(right_sample, col_idx=1))
+            ig = self._information_gain(common_freqs, (left_freqs, right_freqs))
+            if ig > best_ig:
+                best_ig = ig
+                best_split = f_value
+        
+        return best_split, ig
 
