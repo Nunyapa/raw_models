d340ced42fe41f892fdd2156aaff8ad3e0e9d4b8
Nunyapa <halo.cola.115@gmail.com>
2022-04-23 15:08:45 +1000

    Bug fix, change _get_tree_stack method, rid off the idea of post-pruning, add max_leaves control during build_tree

--- DecisionTree/DecisionTree.py
+++ DecisionTree/DecisionTree.py
@@ -2,7 +2,7 @@
 import numpy as np
 # from typing import Iterable, Mapping, Tuple, TypeVar
 # import metrics as m
-from .metrics import *
+from metrics import *
 
 # TODO LIST
 # TODO : Initialization method for uninitialized DT parameters
@@ -46,13 +46,15 @@ class Node:
     def __init__(self,
                  sample_indexes=None,
                  targets=None,
-                 depth=None):
+                 depth=None,
+                 node_number=None):
         self.sample_indexes = sample_indexes
         self.targets = targets
         self.depth = depth
+        self.node_number = node_number
 
-        self.left = None
-        self.right = None
+        self.left = -1
+        self.right = -1
         self.split_column = None
         self.split_value = None
         self.metric_value = None
@@ -84,7 +86,8 @@ class DecisionTree:
         self.metric_method_optimization = METRICS_METHOD_OPTIMIZATION[self.split_metric]
         self.comparison_function = COMPARISON_FUNCTIONS[self.metric_method_optimization]
         self._best_split_initialization = SMALL_CONST if self.metric_method_optimization == 'max' else BIG_CONST
-        self.tree = None
+        self.tree_ = None
+        self.tree_stack_ = []
 
     @staticmethod
     def _sort_matrix(matrix, sort_by_col=0):
@@ -123,7 +126,7 @@ class DecisionTree:
             elif len(right_sample_index) < self.min_sample_size_in_leaf:
                 continue
 
-            parent_freqs = get_freqs(sorted_matrix[1], self.classes)
+            parent_freqs = get_freqs(sorted_matrix[:, 1], self.classes)
             left_freqs = get_freqs(sorted_matrix[left_sample_index, 1], self.classes)
             right_freqs = get_freqs(sorted_matrix[right_sample_index, 1], self.classes)
 
@@ -156,7 +159,54 @@ class DecisionTree:
 
         return left_index, right_index, best_col_for_split, best_split_value, best_metric_value
 
-    def check_sample_suit(self, depth, sample_indexes):
+
+
+    @staticmethod
+    def _get_tree_stack(in_node):
+        stack = [in_node]
+        for node in stack:
+            if node == -1:
+                stack.append(None)
+                stack.append(None)
+            elif node is None:
+                continue
+            else:
+                stack.append(node.left)
+                stack.append(node.right)
+        return stack
+
+    # def _get_leaves(self):
+    #     stack = self._get_tree_stack(self.tree)
+    #     leaves_idxs = []
+    #     for idx, current_node in enumerate(stack):
+    #         if current_node.is_leaf():
+    #             leaves_idxs.append(idx)
+
+    # @staticmethod
+    # def _get_leaf_parent_idx(stack, leaf_idx):
+    #     assert leaf_idx < len(stack)
+    #     is_left = leaf_idx % 2 != 0
+    #     if is_left:
+    #         parent_idx = (leaf_idx - 1) // 2
+    #     else:
+    #         parent_idx = (leaf_idx - 2) // 2
+    #     return parent_idx
+
+    def display_tree(self):
+        stack = self._get_tree_stack(self.tree_)
+        thresholds = []
+        cols = []
+        values = []
+
+        for current_node in stack:
+            if isinstance(current_node, Node):
+                thresholds.append(current_node.split_value)
+                cols.append(current_node.split_column)
+                values.append(np.mean(current_node.targets))
+
+        return cols, thresholds, values
+
+    def check_sample_suit(self, depth, sample_indexes, leaves_counter):
         is_sample_suitable = 1
         sample_size = len(sample_indexes)
 
@@ -164,41 +214,20 @@ class DecisionTree:
             is_sample_suitable = 0
         elif sample_size <= self.min_split_sample:
             is_sample_suitable = 0
+        elif (self.max_leaves - leaves_counter) <= 2:
+            is_sample_suitable = 0
 
         return is_sample_suitable
 
-    @staticmethod
-    def _get_tree_stack(in_node):
-        stack = [in_node]
-        for node in stack:
-            stack.append(node.left)
-            stack.append(node.right)
-        return stack
-
-    def display_tree(self):
-        stack = self._get_tree_stack(self.tree)
-        thresholds = []
-        cols = []
-        values = []
-
-        for current_node in stack:
-            thresholds.append(current_node.split_value)
-            cols.append(current_node.split_column)
-            values.append(np.mean(current_node.targets))
-            if not current_node.is_leaf():
-                stack.append(current_node.right)
-                stack.append(current_node.left)
-
-        return cols, thresholds, values
-
     def build_tree(self, sample, targets):
-        stack = []
         sample_indexes = np.array(range(sample.shape[0]))
-        root = Node(sample_indexes=sample_indexes, targets=targets, depth=1)
-        stack.append(root)
+        root = Node(sample_indexes=sample_indexes, targets=targets, depth=1, node_number=0)
 
-        while stack:
-            current_node = stack.pop()
+        self.tree_stack_.append(root)
+
+        leaves_counter = 0
+
+        for current_node in self.tree_stack_:
             split_params = self._get_best_node(sample, targets, current_node.sample_indexes)
             left_index, right_index, best_col, split_value, best_metric = split_params
 
@@ -208,55 +237,59 @@ class DecisionTree:
 
             next_depth = current_node.depth + 1
 
+            parent_node_number = current_node.node_number
             current_node.left = Node(sample_indexes=left_index,
                                      targets=targets[left_index],
-                                     depth=next_depth)
+                                     depth=next_depth,
+                                     node_number=parent_node_number + 1)
 
             current_node.right = Node(sample_indexes=right_index,
                                       targets=targets[right_index],
-                                      depth=next_depth)
+                                      depth=next_depth,
+                                      node_number=parent_node_number + 2)
 
-            if self.check_sample_suit(next_depth, current_node.right.sample_indexes):
-                stack.append(current_node.right)
+            if self.check_sample_suit(next_depth, left_index, leaves_counter):
+                self.tree_stack_.append(current_node.left)
+            else:
+                leaves_counter += 1
 
-            if self.check_sample_suit(next_depth, current_node.left.sample_indexes):
-                stack.append(current_node.left)
+            if self.check_sample_suit(next_depth, right_index, leaves_counter):
+                self.tree_stack_.append(current_node.right)
+            else:
+                leaves_counter += 1
+
         return root
 
-    def prune_tree(self):
-        stack = self._get_tree_stack(self.tree)
-        leaves_idxs = []
-        for idx, current_node in enumerate(stack):
-            if current_node.is_leaf():
-                leaves_idxs.append(idx)
+    # def prune_tree(self):
+    #     stack = self._get_tree_stack(self.tree)
+    #     leaves_idxs = self._get_leaves()
+    #     leaves_to_prune = []
+    #
+    #     while len(leaves_idxs) > self.max_leaves:
+    #         worst_leaf_idx = self._get_worst_leaf(stack, leaves_idxs)
+    #         leaves_to_prune.append(worst_leaf_idx)
+    #         leaves_idxs.remove(worst_leaf_idx)
+    #
+    #     # TODO : To delete a leaf it is not enough to remove it from stack
+    #     # it requires to set to the leaf parent`s left and right attribute None value
+    #     # TODO : split current prune_tree method into 2. 1st is method to get leaves stack,
+    #     # 2nd is to iterate through the leaves parents nodes and set None value to a required node.
+    #     # Thoughts. How to deal with leaf pruning in the situation when a parent node has 2 leaves and one
+    #     # of them has the worst score and another one has the best score for the all current leaves.
 
-        leaves_to_prune = []
-
-        while len(leaves_idxs) > self.max_leaves:
-            worst_leaf_idx = self._get_worst_leaf(stack, leaves_idxs)
-            leaves_to_prune.append(worst_leaf_idx)
-            leaves_idxs.remove(worst_leaf_idx)
-
-        # TODO : To delete a leaf it is not enough to remove it from stack
-        # it requires to set to the leaf parent`s left or right attribute None value
-        # TODO : split current prune_tree method into 2. 1st is method to get leaves stack,
-        # 2nd is to iterate through the leaves parents nodes and set None value to a required node.
-        # Thoughts. How to deal with leaf pruning in the situation when a parent node has 2 leaves and one
-        # of them has the worst score and another one has the best score for the all current leaves.
-
-    def _get_worst_leaf(self, stack, leaves_idxs):
-        worst_leaf_metric = BIG_CONST if self.metric_method_optimization == 'max' else SMALL_CONST
-        worst_leaf_idx = SMALL_CONST
-        for current_leaf_idx in leaves_idxs:
-            if worst_leaf_metric > stack[current_leaf_idx].metric_value:
-                worst_leaf_metric = stack[current_leaf_idx].metric_value
-                worst_leaf_idx = current_leaf_idx
-        return worst_leaf_idx
+    # def _get_worst_leaf(self, stack, leaves_idxs):
+    #     worst_leaf_metric = BIG_CONST if self.metric_method_optimization == 'max' else SMALL_CONST
+    #     worst_leaf_idx = SMALL_CONST
+    #     for current_leaf_idx in leaves_idxs:
+    #         if worst_leaf_metric > stack[current_leaf_idx].metric_value:
+    #             worst_leaf_metric = stack[current_leaf_idx].metric_value
+    #             worst_leaf_idx = current_leaf_idx
+    #     return worst_leaf_idx
 
     def fit(self, X_train, y_train):
         # assert self.classes == len(np.unique(y_train))
         self.classes = np.unique(y_train)
-        self.tree = self.build_tree(X_train, y_train)
+        self.tree_ = self.build_tree(X_train, y_train)
 
     def _node_predict(self, node, sample, indexes):
         if node.is_leaf():
@@ -285,7 +318,7 @@ class DecisionTree:
 
     def predict(self, data):
         indexes = np.array([_ for _ in range(data.shape[0])])
-        results, parent_indexes = self._node_predict(self.tree, data, indexes)
+        results, parent_indexes = self._node_predict(self.tree_, data, indexes)
         # print(len(results), len(parent_indexes))
         sorted_results = np.column_stack([parent_indexes, results])
         sorted_results = self._sort_matrix(sorted_results, sort_by_col=0)
--- DecisionTree/test.py
+++ DecisionTree/test.py
@@ -1,17 +1,18 @@
 import numpy as np
 from DecisionTree import *
 
-sample = np.random.randn(10, 15)
+sample = np.random.randn(100, 15)
 # print(sample[:, 0])
-targets = np.random.randint(0, 2, size=10)
+targets = np.random.randint(0, 2, size=100)
 
 tree = DecisionTree(max_depth=5,
-                max_leaves=32,
-                min_sample_size_in_leaf=3,
-                min_split_sample=6,
-
-                split_metric='ig')
+                    max_leaves=32,
+                    min_sample_size_in_leaf=3,
+                    min_split_sample=6,
+                    split_metric='ig')
 
 tree.fit(sample, targets)
-print(tree.display_tree())
+print(tree.display_tree()[0])
+print(tree._get_tree_stack(tree.tree_))
+print(len(tree._get_tree_stack(tree.tree_)))
 
