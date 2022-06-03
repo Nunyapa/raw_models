ff8118d142241353a7656dee8370cfa3a4d3da70
Nunyapa <halo.cola.115@gmail.com>
2022-06-03 11:02:09 +1000

    add fit. add predict. refresh todo list

--- Bagging/Bagging.py
+++ Bagging/Bagging.py
@@ -1,4 +1,5 @@
 import numpy as np
+from DecisionTree.DecisionTree import DecisionTreeClassifier
 
 
 class BaggingClass:
@@ -14,29 +15,86 @@ class BaggingClass:
                  tree_params,
                  iterations,
                  bagging_size_ratio,
-                 features_size_ratio):
+                 features_size_ratio,
+                 random_state=42):
+        # TODO: add early_stopping_rounds and maybe another important params.
 
         self.tree_params = tree_params
         self.iterators = iterations
         self.bagging_size_ratio = bagging_size_ratio
         self.features_size_ratio = features_size_ratio
+        self.random_state = random_state
+
+        self.trees_info = None
 
     def sample_with_replacement(self, X, y):
         features_size = round(self.features_size_ratio * X.shape[1])
         features_size = 1 if features_size == 0 else features_size
 
-        features = np.random.choice(np.array(range(X.shape[1])), size=features_size, replace=False)
+        sub_features = np.random.RandomState(self.random_state).choice(np.array(range(X.shape[1])),
+                                                                   size=features_size,
+                                                                   replace=False)
 
-        parent_sample = np.dstack([X[:, features], y])[0]
+        parent_sample = np.dstack([X[:, sub_features], y])[0]
         sub_sample_size = round(self.bagging_size_ratio * X.shape[0])
 
-        sub_sample = np.random.choice(parent_sample, size=sub_sample_size, replace=True)
+        sub_sample = np.random.RandomState(self.random_state).choice(parent_sample,
+                                                                     size=sub_sample_size,
+                                                                     replace=True)
+
+        sub_sample_info = {
+            'features': sub_features
+        }
+
         sub_sample_X = sub_sample[:, :-1]
         sub_sample_y = sub_sample[:, -1]
 
-        return sub_sample_X, sub_sample_y
+        return sub_sample_X, sub_sample_y, sub_sample_info
+
+    def predict(self, X):
+        if self.trees_info is None:
+            raise ValueError('self.trees_info is None. Call fit first')
+
+        iterators_list = range(self.iterators)
+        results = []
+        for tree_idx in iterators_list:
+            tree = self.trees_info[tree_idx]['tree']
+            sub_sample_info = self.trees_info[tree_idx]['tree_info']
+            sub_features = sub_sample_info['features']
+
+            result = tree.predict(X[:, sub_features])
+
+            results.append(result)
+
+        results = np.array([results])
+        results = np.median(results, axis=1)
+
+        return results
+
+
+    def compute_metric(self):
+        pass
+
+    def _fit(self, X, y, eval_set=(None, None)):
+        self.trees_info = {}
+        iterators_list = range(self.iterators)
+
+        for tree_idx in iterators_list:
+            current_tree = DecisionTreeClassifier(**self.tree_params)
+            sub_sample_X, sub_sample_y, sub_sample_info = self.sample_with_replacement(X, y)
+
+            current_tree.fit(sub_sample_X, sub_sample_y)
+
+
+            tree_info_dict = {
+                'tree': current_tree,
+                'tree_info': sub_sample_info
+            }
+
+            self.trees_info[tree_idx] = tree_info_dict
+
 
     def fit(self, X, y, eval_set=(None, None)):
         # TODO: check for eval set
-        # TODO: Voting system. (multiple trees will yeild predicts and its needed to weigh them all in order to get one array of predicts)
+        # TODO: Want to stop training if self.early_stopping steps was reached without metric improvement.
         pass
